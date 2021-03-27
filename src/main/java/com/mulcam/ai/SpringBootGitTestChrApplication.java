d2f7b24bca8b8ffa8dd3222f9b20641164eceff0
ChoiHyeRin <lyyyn@naver.com>
2021-03-27 13:51:45 +0900

    [최혜린] 최초 프로젝트 구성

---
+++ src/main/java/com/mulcam/ai/SpringBootGitTestChrApplication.java
@@ -0,0 +1,13 @@
+package com.mulcam.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitTestChrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitTestChrApplication.class, args);
+	}
+
+}
